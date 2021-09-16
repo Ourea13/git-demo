45b573d2b3a54820c1dfee87488d4ca4e297cebb
onirii13 <onirii13@gmail.com>
2021-09-17 00:35:23 +0800

    hotfix first commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
         System.out.println("Hello World");
         System.out.println("Git");
         System.out.println("Spring Boot");
+        System.out.println("Redis");
     }
 }
